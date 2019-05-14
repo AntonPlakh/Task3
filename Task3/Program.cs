30ef38eb9984bc120589e2f46b3db61886d64027
AntonPlakh <50624101+AntonPlakh@users.noreply.github.com>
2019-05-14 19:02:27 +0300

    Update Program.cs

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -10,6 +10,55 @@ namespace Task3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter first number from range");
+            int firstEl;
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out firstEl) && firstEl > 0)
+                {
+                    break;
+                }
+                else Console.WriteLine("Try one more time");
+            }
+
+
+
+
+
+            Console.WriteLine("Please enter second number from range");
+            int lastEl;
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out lastEl) && lastEl > firstEl)
+                {
+                    break;
+                }
+                else Console.WriteLine("Try one more time");
+            }
+
+
+            Console.WriteLine("first = {0} second= {1}", firstEl, lastEl);
+
         }
+
+
+        public static int Calculation(int firstEl, int lastEl)
+
+        {
+            int sum = 0;
+            for (int i = firstEl; i < lastEl; i++)
+            {
+
+                if ((i % 3 == 0) && (i % 5 != 0))
+                    sum = sum + i;
+
+            }
+            return sum;
+        }
+
+
+
     }
 }
+
+
